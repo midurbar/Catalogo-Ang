9152887daab0fb632176be34a9c6dbf32c8942af
midurbar <midurbar@gmail.com>
2020-12-18 20:06:07 +0100

    añadido formulario ractivo novelas

--- src/app/novela-detail/novela-detail.component.ts
+++ src/app/novela-detail/novela-detail.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-novela-detail',
@@ -15,9 +18,33 @@ export class NovelaDetailComponent implements OnInit {
     descrp: null
   })
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder, private http: HttpClient, private route:ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      if (params.id !== '_nuevo') {
+        this.http.get<any>(environment.apiBaseUrl + "novelas/" + params.id)
+        .subscribe(
+          novela => {
+            this.novelaForm.patchValue(novela)
+          },
+          error => {
+            alert("Error cargando la novela")
+          }
+        )
+      }
+    })
+  }
+
+  crear() {
+    this.http.post(environment.apiBaseUrl + "novelas", this.novelaForm.value)
+      .subscribe(
+        novela => {
+          alert("Novela creada con éxito")
+        },
+        error => {
+          alert("Error creando la novela: " + error.message)
+        })
   }
 
 }
